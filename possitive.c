b06b2c5476b2ce874c26e5d9f7779f924079e8e3
devisri123 <35295385+devisri123@users.noreply.github.com>
2018-01-10 17:00:21 +0530

    possitive.c

---
+++ possitive.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+voidmain()
+{
+int num;
+printf("enter a number:\n")
+scanf("%d",\num);
+if(num>0)
+printf("%d"is a possitive number\n",num);
+else if (num<0)
+printf("%d is negative number\n");
+else
+printf("0 is possitive nor negative");
+}
